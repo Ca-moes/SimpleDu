e3c0c12a80b08591e5c0b691bb8a7a091bdc4031
Andre_G <andre3000pt@gmail.com>
2020-03-25 14:17:30 +0000

    Organizing

--- run.bash
+++ run.bash
@@ -4,9 +4,13 @@ echo "Mudar as flags neste -bash"
 echo "__________________________"
 echo
 
+cd src
+
 if gcc -Wall -o simpledu simpledu.c; then
 ./simpledu -a -aa
 else 
 echo "COMPILATION ERROR";
 fi 
 
+# apaga todos os binarios criados por gcc
+find . -type f  ! -name "*.*"  -delete
