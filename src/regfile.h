709286c944964bcbad02c876a71a5ae45a69bf75
filipeeteixeira <filipeteixeira00@hotmail.com>
2020-03-26 16:32:58 +0000

    Implemented functions to log actions on file

---
+++ src/regfile.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/types.h>
+#include <inttypes.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+
+void initRegister();
+
+/*
+function to log the command in the file
+*/
+void regCommand(int argc,char const *argv[]);
+
+/*
+function to make a new process and log it in the file
+*/
+pid_t regFork();
+
+/*
+function to exit a process and log it in the file
+*/
+void regExit(int status);
+
+/*
+function to receive a signal and log it in the file
+this function will be the signal_handler
+*/
+void regReceiveSignal(int signal);
+
+/*
+function to send a signal and log it in the file
+*/
+int regSendSignal(pid_t dpid,int signo);
+
+/*
+function to send a message using pipe and log it in the file
+*/
+int regSendMessage(int fd, void *buf, size_t n);
+
+/*
+function to receive a message using pipe and log it in the file
+*/
+int regReceiveMessage(int fd, void *buf, size_t n);
