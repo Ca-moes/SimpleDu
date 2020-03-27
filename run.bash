92c1b3571e4e477c7d0468ffdc64c475d1d9720b
Andre_G <andre3000pt@gmail.com>
2020-03-27 11:27:53 +0000

    MakeFile and bash updated

--- run.bash
+++ run.bash
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-echo "Mudar as flags neste -bash"
-echo "__________________________"
-echo
+echo "CHANGE FLAGS IN BASH"
 
 cd src
 
-if gcc -Wall -o simpledu simpledu.c regfile.c; then
-./simpledu -l ../TestDir -a --block-size=4096 --max-depth=3 -L -S
-else 
-echo "COMPILATION ERROR";
-fi 
+make 
+echo "----------------------------"
+
+
+# $? = 0 se compilou bem
+# $? = 2 otherwise
+
+if [ $? -eq 0 ] ; then
+  ./simpledu.exe -l ../TestDir -a --block-size=4096 --max-depth=3 -L -S
+else
+  echo "COMPILATION ERROR";
+fi
+
+echo "----------------------------"
+make clean
 
 # apaga todos os binarios criados por gcc
-find . -type f  ! -name "*.*"  -delete
+# find . -type f  ! -name "*.*"  -delete
+# apaga os .o
+# find . -type f -iname \*.o -delete
