3be12e44bb7589fb7c3112f770d8844fd7dca38a
Diogo Nunes <diogoabnunes@gmail.com>
2020-03-29 11:47:54 +0100

    considering directories

--- src/scan.c
+++ src/scan.c
@@ -14,17 +14,37 @@ int list_reg_files(flags *flags) {
 
   while ((dentry = readdir(dir)) != NULL) {
     stat(dentry->d_name, &stat_entry);
+    
     if (S_ISREG(stat_entry.st_mode)) {
 
       if (flags->bytes) {
         int fileSize = stat_entry.st_size;
-        printf("%-d\t%-25s\n", fileSize, dentry->d_name);
+        printf("%-d\t./%-25s\n", fileSize, dentry->d_name);
       }
 
       else {
         int fileSize = stat_entry.st_size;
         int numBlocks = fileSize / stat_entry.st_blksize;
-        printf("%-d\t%-25s\n", numBlocks, dentry->d_name);
+        printf("%-d\t./%-25s\n", numBlocks, dentry->d_name);
+      }
+    }
+    if (S_ISDIR(stat_entry.st_mode)) {
+      if (strcmp(dentry->d_name, "..") == 0) continue;
+
+      if (strcmp(dentry->d_name, ".") == 0) {
+        int folderSize = stat_entry.st_size;
+        printf("%-d\t%-25s\n", folderSize, dentry->d_name);
+      }
+
+      else if (flags->bytes) {
+        int folderSize = stat_entry.st_size;
+        printf("%-d\t./%-25s\n", folderSize, dentry->d_name);
+      }
+
+      else {
+        int folderSize = stat_entry.st_size;
+        int numBlocks = folderSize / stat_entry.st_blksize;
+        printf("%-d\t./%-25s\n", numBlocks, dentry->d_name);
       }
     }
   }
